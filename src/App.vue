9563032a72f096035199546a7806dc3a0e829d56
cyberfolk <andrea1995ferrari.com>
2023-04-14 01:28:58 +0200

    Add alt in img and remove align-center in .banner

--- src/App.vue
+++ src/App.vue
@@ -43,23 +43,23 @@ export default {
       <div class="container">
         <nav class="nav d-flex justify-content-around">
           <a class="nav-link" href="#">
-            <img src="./assets/img/buy-comics-digital-comics.png" alt="" />
+            <img src="./assets/img/buy-comics-digital-comics.png" alt="digital-comics" />
             <span>DIGITAL COMICS</span>
           </a>
           <a class="nav-link" href="#">
-            <img src="./assets/img/buy-comics-merchandise.png" alt="" />
+            <img src="./assets/img/buy-comics-merchandise.png" alt="merchandise" />
             <span>DC MERCHANANDISE</span>
           </a>
           <a class="nav-link" href="#">
-            <img src="./assets/img/buy-comics-subscriptions.png" alt="" />
+            <img src="./assets/img/buy-comics-subscriptions.png" alt="subscriptions" />
             <span>SUBSCRIPTION</span>
           </a>
           <a class="nav-link" href="#">
-            <img src="./assets/img/buy-comics-shop-locator.png" alt="" />
+            <img src="./assets/img/buy-comics-shop-locator.png" alt="comics-shop-locator" />
             <span>COMIC SHOP LOCATOR</span>
           </a>
           <a class="nav-link d-flex" href="#">
-            <img class="py-2" src="./assets/img/buy-dc-power-visa.svg" alt="" />
+            <img class="py-2" src="./assets/img/buy-dc-power-visa.svg" alt="dc-power-visa" />
             <div class="align-self-center">DC POWER VISA</div>
           </a>
         </nav>
