e8520eb205d6b4eb8f304c4ede75a4ddd70e7c60
cyberfolk <andrea1995ferrari.com>
2023-04-13 19:17:44 +0200

    Add col in footer_top

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,59 @@ export default {
     <div class="footer_up">
       <div class="container">
         <div class="row">
-          <div class="col-6"></div>
+          <div class="col-6">
+            <div class="row">
+              <div class="col-3">
+                <h2>DC COMICS</h2>
+                <ul>
+                  <li><a href="#">Characters</a></li>
+                  <li><a href="#">Comics</a></li>
+                  <li><a href="#">Movies</a></li>
+                  <li><a href="#">TV</a></li>
+                  <li><a href="#">Games</a></li>
+                  <li><a href="#">Videos</a></li>
+                  <li><a href="#">News</a></li>
+                </ul>
+                <h2>SHOP</h2>
+                <ul>
+                  <li><a href="#">Shop DC</a></li>
+                  <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+              </div>
+              <!-- /.col-3 -->
+
+              <div class="col-3">
+                <h2>DC</h2>
+                <ul>
+                  <li><a href="#">Term Of Use</a></li>
+                  <li><a href="#">Privacy polici (News)</a></li>
+                  <li><a href="#">Ad Choices</a></li>
+                  <li><a href="#">Advertising</a></li>
+                  <li><a href="#">Jobs</a></li>
+                  <li><a href="#">Subscriptions</a></li>
+                  <li><a href="#">Talent Workshops</a></li>
+                  <li><a href="#">CPSC Certificates</a></li>
+                  <li><a href="#">Ratings</a></li>
+                  <li><a href="#">Shop Help</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <!-- /.col-3 -->
+
+              <div class="col-3">
+                <h2>SITES</h2>
+                <ul>
+                  <li><a href="#">DC</a></li>
+                  <li><a href="#">MAD Magazine</a></li>
+                  <li><a href="#">DC Kids</a></li>
+                  <li><a href="#">DC Universe</a></li>
+                  <li><a href="#">DC Power Visa</a></li>
+                </ul>
+              </div>
+              <!-- /.col-3 -->
+            </div>
+            <!-- /.row -->
+          </div>
           <!-- /.col-6 -->
           <div class="col-6">
             <img src="./assets/img/dc-logo-bg.png" alt="" />
@@ -119,9 +171,29 @@ main {
 
 footer {
   .footer_up {
+    padding-top: 2rem;
     background-image: url("./assets/img/footer-bg.jpg");
     background-size: cover;
     height: 400px;
+    color: $light;
+
+    h2 {
+      font-family: "Bahnschrift SemiBold";
+      font-size: 1.5rem !important;
+    }
+
+    ul {
+      padding: 0;
+      li {
+        list-style: none;
+        a {
+          text-decoration: none;
+          color: $secondary;
+          font-size: 0.8rem;
+        }
+      }
+    }
+
     img {
       position: relative;
       bottom: 15%;
