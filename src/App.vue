101e4e4dcb61a71f21cb8ec43242314645ae18fd
cyberfolk <andrea1995ferrari.com>
2023-04-13 17:28:47 +0200

    Add header

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,61 @@ export default {
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <header class="mt-4 bg-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-4 py-3 d-flex">
+          <img class="align-self-center" src="./assets/img/dc-logo.png" alt="dc-log" />
+        </div>
+        <!-- /.col -->
+        <div class="col-8 d-flex">
+          <nav class="nav align-self-center">
+            <a class="nav-link" href="#" aria-current="page">CHARACTERS</a>
+            <a class="nav-link" href="#">COMICS</a>
+            <a class="nav-link" href="#">MOVIES</a>
+            <a class="nav-link" href="#">TV</a>
+            <a class="nav-link" href="#">GAMES</a>
+            <a class="nav-link" href="#">COLLECTIBLES</a>
+            <a class="nav-link" href="#">VIDEOS</a>
+            <a class="nav-link active" href="#">FANS</a>
+            <a class="nav-link" href="#">NEWS</a>
+            <a class="nav-link" href="#">SHOP</a>
+          </nav>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+  </header>
 </template>
 
 <style lang="scss">
-//@use "./assets/scss/partials/variables" as *;
-h1 {
-  color: red;
+@use "./assets/scss/partials/variables" as *;
+header {
+  a {
+    font-family: "Bahnschrift SemiBold";
+    font-size: 0.9rem !important;
+    color: $dark !important;
+    position: relative;
+  }
+
+  .active {
+    color: $accenture !important;
+  }
+
+  .active:after {
+    content: "";
+    position: absolute;
+    right: 0;
+    bottom: -100%;
+    height: 6px;
+    width: 100%;
+    background-color: $accenture;
+  }
+
+  img {
+    height: 80px;
+  }
 }
 </style>
