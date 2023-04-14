0651043825ce2a00e5e09a30e5bc52b57a6ede7b
cyberfolk <andrea1995ferrari.com>
2023-04-14 15:13:01 +0200

    Use :vfor to popolate BannerVue component

---
+++ src/components/BannerVue.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: "BannerVue",
+  components: {},
+  data() {
+    return {
+      banner: [
+        {
+          name: "DIGITAL COMICS",
+          src: "buy-comics-digital-comics.png",
+        },
+        {
+          name: "DC MERCHANANDISE",
+          src: "buy-comics-merchandise.png",
+        },
+        {
+          name: "SUBSCRIPTION",
+          src: "buy-comics-subscriptions.png",
+        },
+        {
+          name: "COMIC SHOP LOCATOR",
+          src: "buy-comics-shop-locator.png",
+        },
+        {
+          name: "DC POWER VISA",
+          src: "buy-dc-power-visa.svg",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    getImageUrl(name) {
+      return new URL(`../assets/img/${name}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <section class="banner">
+    <div class="container">
+      <nav class="nav d-flex justify-content-around">
+        <a class="nav-link d-flex" href="#" v-for="item in banner">
+          <img :src="getImageUrl(item.src)" alt="digital-comics" />
+          <div class="align-self-center">{{ item.name }}</div>
+        </a>
+      </nav>
+    </div>
+    <!-- /.container -->
+  </section>
+  <!-- /.banner -->
+</template>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,7 +1,10 @@
 <script>
+import BannerVue from "./BannerVue.vue";
 export default {
   name: "SiteMain",
-  components: {},
+  components: {
+    BannerVue,
+  },
 };
 </script>
 
@@ -12,33 +15,7 @@ export default {
       <!-- /.container -->
     </section>
     <!-- /.content -->
-    <section class="banner">
-      <div class="container">
-        <nav class="nav d-flex justify-content-around">
-          <a class="nav-link" href="#">
-            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics" />
-            <span>DIGITAL COMICS</span>
-          </a>
-          <a class="nav-link" href="#">
-            <img src="../assets/img/buy-comics-merchandise.png" alt="merchandise" />
-            <span>DC MERCHANANDISE</span>
-          </a>
-          <a class="nav-link" href="#">
-            <img src="../assets/img/buy-comics-subscriptions.png" alt="subscriptions" />
-            <span>SUBSCRIPTION</span>
-          </a>
-          <a class="nav-link" href="#">
-            <img src="../assets/img/buy-comics-shop-locator.png" alt="comics-shop-locator" />
-            <span>COMIC SHOP LOCATOR</span>
-          </a>
-          <a class="nav-link d-flex" href="#">
-            <img class="py-2" src="../assets/img/buy-dc-power-visa.svg" alt="dc-power-visa" />
-            <div class="align-self-center">DC POWER VISA</div>
-          </a>
-        </nav>
-      </div>
-      <!-- /.container -->
-    </section>
-    <!-- /.banner -->
+
+    <BannerVue />
   </main>
 </template>
