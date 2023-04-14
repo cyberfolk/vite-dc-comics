bd384e827b525b9dfa528b206c8dd31ef43b0c75
cyberfolk <andrea1995ferrari.com>
2023-04-14 15:43:07 +0200

    Add some text in main to show dark section

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -10,6 +10,8 @@ export default {
 
 <template>
   <main>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magnam inventore vel tempora alias adipisci sequi aperiam provident. Ab necessitatibus numquam, vero dolorum esse sint, dolore dolor velit rem laudantium facere blanditiis. Molestiae animi nobis dolor reiciendis blanditiis? Voluptatibus unde, quisquam deleniti laudantium minima delectus velit dolorum magnam nulla ad aperiam, repellat necessitatibus modi, nihil harum quo assumenda! Consectetur deleniti, voluptate error explicabo perspiciatis eos voluptatum culpa sit, eum consequatur quaerat quae perferendis ex quia? Suscipit, quam rem voluptas sint voluptatum pariatur odio. Est temporibus praesentium sed alias aut? Explicabo nesciunt perferendis quae quam, quasi nam voluptates cum eos, obcaecati eius totam similique vel voluptate dolorum. Dolore minima ea beatae nulla, minus tempore eveniet doloremque laudantium amet odit, fuga, recusandae fugiat tenetur quia doloribus mollitia enim blanditiis adipisci aut nam! Deleniti,
+    maxime odit pariatur distinctio, vel adipisci quaerat architecto ab non laborum, molestiae tempora cum quod molestias aliquid cupiditate eaque!
     <div class="container"></div>
     <!-- /.container -->
 
