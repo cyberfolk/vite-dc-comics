ae1e1a97b9325362f2bf572f515c1954542e561c
cyberfolk <andrea1995ferrari.com>
2023-04-14 15:27:26 +0200

    Use :vfor to popolate SiteHeader component

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -2,6 +2,62 @@
 export default {
   name: "SiteHeader",
   components: {},
+  data() {
+    return {
+      pages: [
+        {
+          name: "CHARACTERS",
+          href: "#",
+        },
+        {
+          name: "COMICS",
+          href: "#",
+        },
+        {
+          name: "MOVIES",
+          href: "#",
+        },
+        {
+          name: "TV",
+          href: "#",
+        },
+        {
+          name: "GAMES",
+          href: "#",
+        },
+        {
+          name: "COLLECTIBLES",
+          href: "#",
+        },
+        {
+          name: "GAMES",
+          href: "#",
+        },
+        {
+          name: "VIDEOS",
+          href: "#",
+        },
+        {
+          name: "FANS",
+          href: "#",
+        },
+        {
+          name: "NEWS",
+          href: "#",
+        },
+        {
+          name: "SHOP",
+          href: "#",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    getImageUrl(name) {
+      return new URL(`../assets/img/${name}`, import.meta.url).href;
+    },
+  },
 };
 </script>
 
@@ -19,35 +75,8 @@ export default {
         <!-- /.navbar-toggler -->
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">CHARACTERS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">COMICS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">MOVIES</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">TV</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">GAMES</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">COLLECTIBLES</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">VIDEOS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">FANS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">NEWS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">SHOP</a>
+            <li class="nav-item" v-for="page in pages">
+              <a class="nav-link" :href="page.href">{{ page.name }}</a>
             </li>
           </ul>
         </div>
