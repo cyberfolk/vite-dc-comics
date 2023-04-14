1cc24bee37ccb9d460eb0927c37a4cebca8952f0
cyberfolk <andrea1995ferrari.com>
2023-04-14 17:50:32 +0200

    Add JumbotronVue

---
+++ src/components/JumbotronVue.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "JumbotronVue",
+};
+</script>
+
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<style scoped>
+.jumbotron {
+  background-image: url("../assets/img/jumbotron.jpg");
+  background-size: cover;
+  height: 400px;
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,17 +1,20 @@
 <script>
 import BannerVue from "./BannerVue.vue";
 import ProductList from "./ProductList.vue";
+import JumbotronVue from "./JumbotronVue.vue";
 export default {
   name: "SiteMain",
   components: {
     BannerVue,
     ProductList,
+    JumbotronVue,
   },
 };
 </script>
 
 <template>
   <main>
+    <JumbotronVue />
     <div class="container">
       <ProductList />
     </div>
