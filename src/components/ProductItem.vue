55faf117b60edec0ddead35864c163e8270b557e
cyberfolk <andrea1995ferrari.com>
2023-04-14 17:13:29 +0200

    Show somethings with ProductItems

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "ProductItem",
+  props: {
+    src: String,
+  },
+};
+</script>
+
+<template>
+  <div class="col">
+    <div class="product">
+      <img :src="src" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  height: 180px;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<script>
+import dc_comicss from "../data/dc-comics.js";
+import ProductItem from "./ProductItem.vue";
+
+export default {
+  name: "ProductList",
+  components: {
+    ProductItem,
+  },
+  data() {
+    return {
+      dc_comicss: dc_comicss,
+    };
+  },
+
+  methods: {
+    getImageUrl(nome) {
+      return new URL(`../assets/img/${nome}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="row row-cols-2 row-cols-lg-6 g-2 g-lg-3 py-5">
+    <ProductItem :src="comic.thumb" v-for="comic in dc_comicss" />
+  </div>
+</template>
